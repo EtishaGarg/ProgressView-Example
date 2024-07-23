4c22d284db17bfe6c2d370e07339566d76752ae2
Etishagarg <gargetisha@gmail.com>
2024-07-23 12:05:14 +0530

    Initial Commit

---
+++ ProgressView-Example/ProgressView_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressView_ExampleApp.swift
+//  ProgressView-Example
+//
+//  Created by Etisha Garg on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ProgressView_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
